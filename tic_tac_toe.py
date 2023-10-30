b9c4319831613b47be24e14fe3f22e0ad80a404e
ZacG1t <teozhelun@gmail.com>
2023-10-30 20:55:45 +0800

    improve side selection code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,9 +5,9 @@ print("Welcome to my tic-tac-toe.")
 
 # Game board
 print("\nThis is the game board.")
-board = [['-','-','-'],
-         ['-','-','-'],
-         ['-','-','-']]
+board = [['1','2','3'],
+         ['4','5','6'],
+         ['7','8','9']]
 
 for row in board: 
     print(row)
@@ -15,14 +15,15 @@ for row in board:
 # Choosing side 
 side = input("Choose your side: X / O\n")
 
-while side != ('X' or 'O'):
-    if side == 'X':
-        print("You are X.")
-        break
-    elif side == 'O':
-        print("You are O.")
-        break
-    else:
-        side = input("Please choose by typing 'X' or 'O'.\n")
+#while side != ('X' or 'O'):
+if side != ('X' or 'O'):
+    side = input("Please choose by typing 'X' or 'O'.\n")
+elif side == 'X':
+    print("You are X.")
+    #break
+elif side == 'O':
+    print("You are O.")
+    #break
 
 print("Let the game begin. :)")
+
